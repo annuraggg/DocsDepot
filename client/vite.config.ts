624c67af29f05decca5353fdefec71e8d7a86730
Anurag <anuragsawant@duck.com>
2025-06-04 19:50:51 +0530

    fix: update Vite configuration to load environment variables dynamically

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASENAME || '/',
-  plugins: [react(), tsconfigPaths()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASENAME || "/",
+    plugins: [react(), tsconfigPaths()],
+  };
+});
